4241c552e19ddac10d4d8e0b359d6884f692b39d
adithya <adithyaiman07@gmail.com>
2025-07-29 09:42:13 +0530

    Re correct folder architecture

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LandingComponent } from './landing/landing.component';
-import { LoginComponent } from './login/login.component';
-import { SignupComponent } from './signup/signup.component';
-import { BlogComponent } from './blog/blog.component';
+import { LandingComponent } from './layout/landing/landing.component';
+import { LoginComponent } from './layout/login/login.component';
+import { SignupComponent } from './layout/signup/signup.component';
+import { BlogComponent } from './layout/blog/blog.component';
 
 const routes: Routes = [
   { path: '', component: LandingComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { LandingComponent } from './landing/landing.component';
-import { LoginComponent } from './login/login.component';
-import { SignupComponent } from './signup/signup.component';
-import { BlogComponent } from './blog/blog.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LandingComponent } from './layout/landing/landing.component';
+import { LoginComponent } from './layout/login/login.component';
+import { SignupComponent } from './layout/signup/signup.component';
+import { BlogComponent } from './layout/blog/blog.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { BlogComponent } from './blog/blog.component';
     LandingComponent,
     LoginComponent,
     SignupComponent,
-    BlogComponent
+    BlogComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
