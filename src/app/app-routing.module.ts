4e36a6d0222a7613564a3ca5a97d62aec64796f5
adithya <adithyaiman07@gmail.com>
2025-07-29 11:39:25 +0530

    Configure authentication for all requests

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { LandingComponent } from './layout/landing/landing.component';
 import { LoginComponent } from './layout/login/login.component';
 import { SignupComponent } from './layout/signup/signup.component';
 import { BlogComponent } from './layout/blog/blog.component';
+import { authGuard } from './auth/auth.guard';
 
 const routes: Routes = [
   { path: '', component: LandingComponent },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
-  { path: 'blog', component:BlogComponent}
+  { path: 'blog', component:BlogComponent,canActivate: [authGuard]},
+  { path: '', redirectTo: '/login', pathMatch: 'full' }
 ];
 
 
--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -34,6 +34,10 @@ export class AuthService {
     return !!localStorage.getItem('token');
   }
 
+  getToken(){
+    localStorage.getItem("token");
+  }
+
 
 
 
