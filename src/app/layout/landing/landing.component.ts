ccdd76ac9107fbc392d631a79ad75cc70a231f09
adithya <adithyaiman07@gmail.com>
2025-07-29 20:49:00 +0530

    Addded landing page search and exapand para

--- src/app/layout/landing/landing.component.ts
+++ src/app/layout/landing/landing.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { BlogModel } from 'src/app/model/blogModel';
+import { BlogModelCreate } from 'src/app/model/BlogModelCreate';
 import { BlogService } from 'src/app/service/blog.service';
 
 @Component({
@@ -9,7 +10,10 @@ import { BlogService } from 'src/app/service/blog.service';
 })
 export class LandingComponent implements OnInit {
 
-  blogs: BlogModel[] = [];
+  blogs: BlogModelCreate [] = [];
+  filteredBlogs: BlogModelCreate[] = [];
+  searchTitle: string = '';
+  expandedBlogId: number | undefined | null = null;
 
   constructor(private blogService: BlogService) {}
 
@@ -21,10 +25,29 @@ export class LandingComponent implements OnInit {
     this.blogService.getAllBlogs().subscribe({
       next: (res) => {
         this.blogs = res.data;
+        this.filteredBlogs = this.blogs;
       },
       error: (err) => {
         console.error('Failed to load blogs:', err);
       }
     });
   }
+
+ filterBlogs() {
+    const search = this.searchTitle.toLowerCase().trim();
+    if (!search) {
+      this.filteredBlogs = this.blogs;
+      return;
+    }
+
+    this.filteredBlogs = this.blogs.filter(blog =>
+      blog.title.toLowerCase().includes(search)
+    );
+  }
+
+  toggleReadMore(blogId: number | undefined) {
+     this.expandedBlogId = this.expandedBlogId === blogId ? null : blogId;
+  }
+
+
 }
